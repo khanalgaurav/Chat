b88d9410739a2d638baa16610835a68365604655
khanalgaurav <khanalgaurav96@gmail.com>
2024-12-17 17:12:10 +0545

    Add cors for frontend port

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,12 +4,20 @@ import messageRoute from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 
